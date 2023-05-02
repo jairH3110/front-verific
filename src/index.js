beb13a016637ec438278a5c9e006a6f3b9854a24
jairH3110 <chiefeon3110@gmail.com>
2023-05-02 10:39:18 -0600

    componiendo workflow

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals'
+
 import { BrowserRouter } from 'react-router-dom';
 
 // 1
